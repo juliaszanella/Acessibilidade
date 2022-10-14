a4780028a279b6b7a765844e9b2fcfa195c8a943
juliaszanella <105882401+juliaszanella@users.noreply.github.com>
2022-10-14 09:41:34 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function contrasteBranco(){
+    var root = document.querySelector(':root');
+    root.style.setProperty('--cor_nav', 'white');
+    root.style.setProperty('--cor_header', 'white');
+    root.style.setProperty('--cor_main', 'white');
+    root.style.setProperty('--cor_fonte01', 'black');
+    root.style.setProperty('--cor_fonte02', 'white');
+    root.style.setProperty('--cor_footer', 'white');
+    root.style.setProperty('--cor_ul', 'black');
+    root.style.setProperty('--cor_compra', 'black');
+    root.style.setProperty('--cor_comprar_hover', 'rgb(20,20,20)');
+    root.style.setProperty('--cor_button_nav', 'black');
+    root.style.setProperty('--cor_button_nav_font', 'white');
+    root.style.setProperty('--cor_font_content', 'black');
+}
+function contrastePreto(){
+    var root = document.querySelector(':root');
+    root.style.setProperty('--cor_nav', 'black');
+    root.style.setProperty('--cor_header', 'black');
+    root.style.setProperty('--cor_main', 'black');
+    root.style.setProperty('--cor_fonte01', 'white');
+    root.style.setProperty('--cor_fonte02', 'black');
+    root.style.setProperty('--cor_footer', 'black');
+    root.style.setProperty('--cor_ul', 'white');
+    root.style.setProperty('--cor_compra', 'white');
+    root.style.setProperty('--cor_comprar_hover', 'rgb(205,205,205)');
+    root.style.setProperty('--cor_button_nav', 'white');
+    root.style.setProperty('--cor_button_nav_font', 'black');
+    root.style.setProperty('--cor_font_content', 'white');
+}
